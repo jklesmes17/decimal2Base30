26ffdf03b5a408fc38fc4ac4e1a46e3f4105b5b5
Nichole Johnn Martinez <njmartinez@fixerinit.co.jp>
2016-01-29 16:43:27 +0800

    John Kevin Lesmes 1st Commit

---
+++ src/main/scala/DecimalToBase.scala
@@ -0,0 +1,7 @@
+package test
+
+trait DecimalToBase {
+   implicit def decimal (hex: Int) = new {
+      def base30: String = Integer.toString(hex, 30)
+    }
+}
---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,10 @@
+package test
+
+object Hello extends DecimalToBase {
+  def main(args: Array[String]): Unit = {
+
+    println(1000.base30.toUpperCase)
+
+  }
+}
+
